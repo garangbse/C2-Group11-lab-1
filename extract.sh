e9f9ee457cd5310c18dca43fb436473b7db8fb02
m-dhieu <monicad@DESKTOP-RP3SUJI.>
2025-02-08 14:37:02 +0200

    Script to extract the temperature and humidity of South Africa.

--- extract.sh
+++ extract.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "$0")"
 
 # Extract top 10 highest temperatures assuming Temperature
-sort -t"," -k3 -rn raw_data/satelite_temperature_data.csv | head -n11 > analyzed_data/highest_temp.csv 
+sort -t"," -k3 -rn raw_data/satelite_temperature_data.csv | head -n10 > analyzed_data/highest_temp.csv 
 
 # Example extraction for South Africa
 awk 'BEGIN{FS=","} $1=="South Africa" {print $0}' raw_data/satelite_temperature_data.csv | sort -t"," -k4,4rn > analyzed_data/humidity_data_South_Africa.csv 
